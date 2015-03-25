34cf3016f06c4fc8051d7002b2a049544375f47d
Cody Engel <cengel815@gmail.com>
2015-03-24 20:36:09 -0500

    Amazon ItemSearch
    Began working on Item Search functionality.

--- index.php
+++ index.php
@@ -10,6 +10,17 @@ require_once("/model/AmazonItem.class.php");
 
 // API Operations http://docs.aws.amazon.com/AWSECommerceService/latest/DG/CHAP_OperationListAlphabetical.html
 
+$search_request = AmazonUtility::AWSSignedRequest(
+		'com', array(
+		'Operation' => "ItemSearch",
+		'Keywords' => "programming",
+		'SearchIndex' => "All",
+		'ResponseGroup' => "ItemIds",
+		'ItemPage' => "10"
+		));
+		
+echo "<a href='".$search_request."'>Click Here!</a>";
+
 $Item1 = new AmazonItem(1479274836);
 $Item2 = new AmazonItem("B00KI03U8Q");
 
